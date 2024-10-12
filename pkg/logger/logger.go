f5457cb15cd5659cbcd222dacac3d0f9bb1f2521
BoxBoxJason <loickgoupil@yahoo.fr>
2024-10-13 00:14:58 +0200

    Add log level to logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -7,29 +7,62 @@ The logger is used to log the application's activity to the console and to a fil
 package logger
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
+	"sync"
 
 	"github.com/boxboxjason/svcadm/internal/static"
 )
 
 // Logger instances
 var (
+	LOG_LEVEL      int
+	mu_LOG_LEVEL   sync.RWMutex
 	debugLogger    *log.Logger
 	infoLogger     *log.Logger
 	errorLogger    *log.Logger
 	criticalLogger *log.Logger
 	fatalLogger    *log.Logger
+	log_levels     = map[string]int{
+		"DEBUG":    0,
+		"INFO":     1,
+		"ERROR":    2,
+		"CRITICAL": 3,
+		"FATAL":    4,
+	}
 )
+
+// SetLogLevel sets the log level
+func SetLogLevel(level string) error {
+	mu_LOG_LEVEL.Lock()
+	defer mu_LOG_LEVEL.Unlock()
+	if _, ok := log_levels[strings.ToUpper(level)]; !ok {
+		return errors.New("invalid log level " + level)
+	}
+	LOG_LEVEL = log_levels[strings.ToUpper(level)]
+	return nil
+}
+
+// GetLogLevel gets the log level
+func GetLogLevel() int {
+	mu_LOG_LEVEL.RLock()
+	defer mu_LOG_LEVEL.RUnlock()
+	return LOG_LEVEL
+}
 
 func init() {
 	// Check if the directory for the log file exists
 	LOG_DIR := fmt.Sprintf("%s/log", static.SVCADM_HOME)
 	LOG_FILE := fmt.Sprintf("%s/svcadm.log", LOG_DIR)
 	if _, err := os.Stat(LOG_DIR); os.IsNotExist(err) {
-		os.Mkdir(LOG_DIR, os.ModePerm)
+		err = os.MkdirAll(LOG_DIR, os.ModePerm)
+		if err != nil {
+			log.Fatalln("Failed to create log directory:", err)
+		}
 	}
 
 	file, err := os.OpenFile(LOG_FILE, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -49,25 +82,35 @@ func init() {
 
 // Debug logs a debug message.
 func Debug(v ...interface{}) {
-	debugLogger.Println(v...)
+	if LOG_LEVEL <= 0 {
+		debugLogger.Println(v...)
+	}
 }
 
 // Info logs an info message.
 func Info(v ...interface{}) {
-	infoLogger.Println(v...)
+	if LOG_LEVEL <= 1 {
+		infoLogger.Println(v...)
+	}
 }
 
 // Error logs an error message.
 func Error(v ...interface{}) {
-	errorLogger.Println(v...)
+	if LOG_LEVEL <= 2 {
+		errorLogger.Println(v...)
+	}
 }
 
 // Critical logs a critical message but does not exit the application.
 func Critical(v ...interface{}) {
-	criticalLogger.Println(v...)
+	if LOG_LEVEL <= 3 {
+		criticalLogger.Println(v...)
+	}
 }
 
 // Fatal logs a fatal message and exits the application.
 func Fatal(v ...interface{}) {
-	fatalLogger.Fatalln(v...)
+	if LOG_LEVEL <= 4 {
+		fatalLogger.Fatalln(v...)
+	}
 }
