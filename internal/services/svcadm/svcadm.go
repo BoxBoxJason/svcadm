83658af616b9f6656ffcade12a62c0b4139a0ea4
BoxBoxJason <loickgoupil@yahoo.fr>
2024-10-13 00:30:38 +0200

    Add service interface

---
+++ internal/services/svcadm/svcadm.go
@@ -0,0 +1,49 @@
+package svcadm
+
+import (
+	"github.com/boxboxjason/svcadm/internal/config"
+	"github.com/boxboxjason/svcadm/pkg/logger"
+)
+
+type ServiceAdm interface {
+	PreInit() (map[string]string, map[string]string, error)
+	PostInit(env_variables map[string]string) error
+	CreateUser(user *config.User) error
+	CreateAdminUser(user *config.User) error
+	Backup(backup_path string) error
+	WaitFor() error
+	GenerateNginxConf() string
+	InitArgs() []string
+	GetService() config.Service
+	ContainerArgs() []string
+}
+
+func Cleanup(service_adm ServiceAdm) (string, []string, []string) {
+	service := service_adm.GetService()
+	volumes := make([]string, 0)
+	files := make([]string, 0)
+
+	return service.Container.Name, volumes, files
+}
+
+func CreateUsers(service_adm ServiceAdm, service_adm_slug string) {
+	users := config.GetUsers()
+	// Create the specified admin users
+	for _, user := range users.Admins {
+		err := (service_adm).CreateAdminUser(&user)
+		if err != nil {
+			logger.Error("could not create the admin user " + user.Username)
+		} else {
+			logger.Debug("created the admin user " + user.Username)
+		}
+	}
+	// Create the specified users
+	for _, user := range users.Users {
+		err := (service_adm).CreateUser(&user)
+		if err != nil {
+			logger.Error("could not create the user " + user.Username)
+		} else {
+			logger.Debug("created the user " + user.Username)
+		}
+	}
+}
