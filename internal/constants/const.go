5c1010c5540f159b58c6ff3e672ba96981468374
BoxBoxJason <loickgoupil@yahoo.fr>
2024-11-08 23:45:47 +0100

    Update path for build on every OS

--- internal/constants/const.go
+++ internal/constants/const.go
@@ -1,13 +1,13 @@
 package constants
 
 import (
-	"fmt"
 	"os"
+	"path"
 )
 
 var (
-	SVCADM_HOME   = fmt.Sprintf("%s/.svcadm", os.Getenv("HOME"))
-	LOG_DIR       = fmt.Sprintf("%s/logs", SVCADM_HOME)
+	SVCADM_HOME   = path.Join(os.Getenv("HOME"), ".svcadm")
+	LOG_DIR       = path.Join(SVCADM_HOME, "logs")
 	SVCADM_LABELS = map[string]string{
 		"managed_by":     "svcadm",
 		"svcadm_version": SVCADM_VERSION,
